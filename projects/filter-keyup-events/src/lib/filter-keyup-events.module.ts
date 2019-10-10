9c6496614b8f2940b8d81d50e30db7385a326c34
swfz <sawafuji.09@gmail.com>
2019-10-11 02:22:41 +0900

    add library filter-keyup-events

---
+++ projects/filter-keyup-events/src/lib/filter-keyup-events.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { FilterKeyupEventsComponent } from './filter-keyup-events.component';
+
+
+
+@NgModule({
+  declarations: [FilterKeyupEventsComponent],
+  imports: [
+  ],
+  exports: [FilterKeyupEventsComponent]
+})
+export class FilterKeyupEventsModule { }
