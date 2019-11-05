c7cc97eacebc4ef892a312720b2c16b10ea7e7f1
swfz <sawafuji.09@gmail.com>
2019-11-05 20:24:13 +0900

    feat: add demo data

--- src/app/pages/filter-keyup-events/filter-keyup-events.component.ts
+++ src/app/pages/filter-keyup-events/filter-keyup-events.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+interface InputEvent {
+  value: string;
+  time: string;
+}
+
 @Component({
   selector: 'app-filter-keyup-events',
   templateUrl: './filter-keyup-events.component.html',
   styleUrls: ['./filter-keyup-events.component.scss']
 })
 export class FilterKeyupEventsComponent implements OnInit {
-  events: string[] = [];
+  events: InputEvent[] = [];
+  intervalMs = 1000;
 
   constructor() {}
 
@@ -14,6 +20,6 @@ export class FilterKeyupEventsComponent implements OnInit {
 
   onFilteredKeyup(event: string) {
     console.log('filtered keyup event');
-    this.events.push(event);
+    this.events.push({value: event, time: (new Date()).getTime().toString() });
   }
 }
